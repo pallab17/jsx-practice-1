fcab71511944890e17183a8a4edef320e2e3fb7c
pallab17 <91554568+pallab17@users.noreply.github.com>
2023-02-23 15:34:16 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 //Create a react app from scratch.
+import React from "react";
+import ReactDOM from "react-dom";
 //It should display a h1 heading.
+//ReactDOM.render(what to show, where to show);
+// ReactDOM.render(<h1> My favorite passtime activities</h1>, document.getElementById("root"));
 //It should display an unordered list (bullet points).
 //It should contain 3 list elements.
+ReactDOM.render(
+  <div>
+    <h1> My favorite passtime activities</h1>
+    <ul>
+      <li>Phone ghata</li>
+      <li>lyadh khawa</li>
+      <li>overthink kora</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
